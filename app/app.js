4de60c24f4c4a774305b05be76c8da795d4a8706
FabVillegas <f.villegas93@gmail.com>
2016-02-05 17:33:25 -0600

    Nested states completed

--- app/app.js
+++ app/app.js
@@ -10,13 +10,25 @@ config( [ '$stateProvider', '$urlRouterProvider', function( $stateProvider, $url
 	$stateProvider.
 	state( 'home', {
 		url: '/home',
-		templateUrl: 'states/home/homeView.html',
-		controller: 'homeController'
+		views: {
+			'': { /* no caracter ni espacios coloca esta view como la default */
+				templateUrl: 'states/home/homeView.html',
+				controller: 'homeController'
+			}
+		}
 	}).
 	state( 'agent', {
 		parent: 'home',
-		url: '/agent',
-		templateUrl: 'states/agent/agentView.html',
-		controller: 'agentController'
+		url: '/agent', /* se le suma al url del padre = '/home' + '/agent' */
+		views: {
+			'': {
+				templateUrl: 'states/agent/agent.view.html',
+				controller: 'agentController'
+			},
+			'profile@agent': { /* @agent hace que ui-view="profile" sea activado solamente si el estado agent esta activo */
+				templateUrl: 'states/agent/profile/agent.profile.view.html',
+				controller: 'agentProfileController'
+			},
+		}
 	});
 }]);
--- app/js_components/directives/agent_profile/agentProfile.js
+++ app/js_components/directives/agent_profile/agentProfile.js
@@ -9,8 +9,10 @@ function agentProfile() {
 		templateUrl: 'js_components/directives/agent_profile/agentProfile.html',
 		link: function( iScope, iElement, iAttrs ) {
 
+			/* vm para usarse como estandar en todos los controllers, factories, services y directives */
 			var vm = iScope;
 
+			/* modelo que podria emplearse para firebase */
 			vm.agent = {
 				name: 'Fernando',
 				lastname_1: 'Garza',
@@ -22,6 +24,20 @@ function agentProfile() {
 				civil_status : 'Don Juan',
 				birthdate: '21-06-1982',
 				rfc: '123456789012',
+				address: 'Vereda de las privanzas #458 Col. Privanzas 4',
+				telephone: '83569845',
+				mobile: '8116459875',
+				email: 'asalinas@gmail.com',
+				last_job: 'Arquitecto',
+				current_job: 'Vendedor',
+				last_department: 'Arquitectura',
+				current_department: 'Ventas',
+				last_monthly_income: 20000,
+				current_monthly_income: 25000,
+				monthly_expenses: 18555,
+				savings: 145500,
+				housing: 'Renta de departamento',
+				monthly_housing_expense: 5000,
 				spouse: {
 					name: 'Blanca',
 					lastname_1: 'Orozco',
@@ -39,6 +55,36 @@ function agentProfile() {
 					agency: 'AXA Seguros',
 					source: 'Bolsa de trabajo',
 					subsource: 'Bolsa de trabajo',
+				},
+				children: {
+					0: {
+						name: 'Jorge',
+						lastname_1: 'Chavez',
+						lastname_2: 'Chavez',
+						birthdate: '08-05-1990',
+						education: 'Estudiante Universidad',
+						current_place: 'ITESM'
+					},
+					1: {
+						name: 'Adrian',
+						lastname_1: 'Chavez',
+						lastname_2: 'Chavez',
+						birthdate: '08-05-1990',
+						education: 'Estudiante Universidad',
+						current_place: 'ITESM'
+					}
+				},
+				vehicles: {
+					0: {
+						brand: 'Honda',
+						model: 'Civic',
+						year: '2009',
+					},
+					1: {
+						brand: 'Honda',
+						model: 'Civic',
+						year: '2009',
+					}
 				}
 			};
 
@@ -53,12 +99,20 @@ function agentProfile() {
 				}
 			}
 
-			vm.disableInputs = true;
 
+			/* estos nombres de las secciones de la informacion
+				deben estar tambien en el template
+				Actualizar aqui, actualizar template */
+			/* editable es para usarse con el ng-show */
 			vm.sections = [
 				{ name: 'informacion-personal', editable: false },
 				{ name: 'control-interno', editable: false },
-				{ name: 'conyuge', editable: false }
+				{ name: 'informacion-contacto', editable: false },
+				{ name: 'conyuge', editable: false },
+				{ name: 'hijos', editable: false },
+				{ name: 'vehiculos', editable: false },
+				{ name: 'economia', editable: false },
+				{ name: 'inmuebles', editable: false }
 			];
 
 			vm.closeThis = function( openedItem ) {
@@ -69,7 +123,8 @@ function agentProfile() {
 				});
 			}
 
-			vm.editThis = function( selectedItem, e ) {
+			/* obtiene el nombre de la seccion, busca y muestra los inputs para editar */
+			vm.editThis = function( selectedItem ) {
 				angular.forEach( vm.sections, function( obj, key ) {
 					if( obj.name === selectedItem ){
 						obj.editable = true;
